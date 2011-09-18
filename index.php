ee3ff9af30063480bf8ac8865029e723b2ef4ea9
Evan Johnson <thaddeusmt@gmail.com>
2011-09-18 16:21:47 -0500

    fix for php warning

--- index.php
+++ index.php
@@ -9,5 +9,7 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
+error_reporting(E_ERROR);
+
 require_once($yii);
 Yii::createWebApplication($config)->run();
