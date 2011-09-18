e5349e04797a58d997216eb5d831f838b58af404
chrisramey <cjramey@gmail.com>
2011-09-18 10:58:08 -0500

    resolved conflict

--- protected/views/app/view.php
+++ protected/views/app/view.php
@@ -1,12 +1,7 @@
 <?php
 $this->breadcrumbs=array(
-<<<<<<< HEAD
-	'Apps'=>array('index'),
-	$model['App']->name,
-=======
 	'Games'=>array('/teacher/games'),
 	$model->name,
->>>>>>> 856dd198b3743249c28442998cafab2099a3810b
 );
 
 /*$this->menu=array(
@@ -17,10 +12,10 @@ $this->breadcrumbs=array(
 	array('label'=>Yii::t('app', 'Manage') . ' App', 'url'=>array('admin')),
 );*/
 ?>
-<<<<<<< HEAD
 <div class="app" style="margin:0px 0px 10px 0px;height:44px;padding:8px;border:1px solid #555;-moz-border-radius:4px;-border-radius:4px;background-color:#efefef">
 	<h1 style="margin:0px;line-height:40px;"><img src="/images/challenges/<?php echo $model['App']->image;?>" alt="App Icon" style="margin-right:10px;float:left;height:40px;" /><?php echo $model['App']->name;?></h1>
 </div>
+
 <?php 
 if(sizeof($model['Challenge']) > 0) { ?>
 	<div class="challenges" style="padding-left:20px">
@@ -37,20 +32,4 @@ if(sizeof($model['Challenge']) > 0) { ?>
 	echo '<p>No challenges found.</p>';
 }?>
 <div class="actions"><a href="">Add Challenge</a></div>
-=======
-
-<h1><?php echo $model->name; ?></h1>
-
-<img src="<?php echo $model->image ?>" />
-
-<h2>Challenges</h2>
-
-<ul>
-<?php foreach($model->challenges as $challenge): ?>
-    <li><?php echo CHtml::link($challenge->name, array('/challenge/edit','id'=>$challenge->id)) ?></li>
-<?php endforeach; ?>
-</ul>
-
-    <h3><?php echo CHtml::link('Create New Challenge', array('/challenge/create')); ?></h3>
-
->>>>>>> 856dd198b3743249c28442998cafab2099a3810b
+<h3><?php echo CHtml::link('Create New Challenge', array('/challenge/create')); ?></h3>
